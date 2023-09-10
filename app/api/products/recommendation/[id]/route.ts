d8b476ac66f8a557f26931a1ba7d479594217f6e
Zen <64142347+zensdev@users.noreply.github.com>
2023-09-10 19:25:32 +0700

    fix rouute recommentdation

--- app/api/products/recommendation/[id]/route.ts
+++ app/api/products/recommendation/[id]/route.ts
@@ -1,6 +1,9 @@
 import { getSimilarProducts } from "@/actions/recommendation";
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
   const data = await getSimilarProducts(params.id);
   return new Response(JSON.stringify(data));
 }
