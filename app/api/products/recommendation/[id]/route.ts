016c60bcf8408d222223973e8744b85822e2cc3c
Zen <64142347+zensdev@users.noreply.github.com>
2023-09-10 19:20:11 +0700

    fix middleware

--- app/api/products/recommendation/[id]/route.ts
+++ app/api/products/recommendation/[id]/route.ts
@@ -1,10 +1,6 @@
 import { getSimilarProducts } from "@/actions/recommendation";
 
-export async function GET(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
-  console.log(params);
+export async function GET({ params }: { params: { id: string } }) {
   const data = await getSimilarProducts(params.id);
   return new Response(JSON.stringify(data));
 }
--- app/middleware.ts
+++ app/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/auctions/:path*"],
+  matcher: ["/auctions/:path*", "/products/product/:path*"],
 };
